7821b378ebbeda819f02c448f50d0791e8c6a25a
Ragulram <ragulram0427@gmail.com>
2025-06-23 20:20:49 +0530

    updateresume api

--- backend/controllers/resumeController.js
+++ backend/controllers/resumeController.js
@@ -121,22 +121,26 @@ export const getResumeById = async (req, res) => {
 //@route PUT /api/resumes/:id
 //@access Private
 export const updateResume = async (req, res) => {
-  try{
-    const resume = await Resume.findOne({
-      _id: req.params.id,
-      userId:req.user._id,
-    });
-    if(!resume){
-      return res.status(404).json({message: "Resume not found"});
+  try {
+    const resume = await Resume.findOneAndUpdate(
+      { _id: req.params.id, userId: req.user._id },
+      req.body,
+      { new: true, runValidators: true }
+    );
+
+    if (!resume) {
+      return res.status(404).json({ message: "Resume not found" });
     }
 
-    Object.assign(resume, req.body);
-    const savedResume = await resume.save();
-    res.json(savedResume);
-
-
-  }catch(error){
-    res.status(500).json({message:"Failed to update resume", error: error.message});
+    res.status(200).json({
+      resume
+    });
+  } catch (error) {
+    console.error("Update Resume Error:", error);
+    res.status(500).json({
+      message: "Failed to update resume",
+      error: error.message
+    });
   }
 }
 
